94c49f4f5b69e95aec8e63b2b5324925ebdafcfc
Anderson Tome <anderson_tome@outlook.com.br>
2022-08-14 01:57:26 -0300

    Exercicio 3a

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Restaurante Food in house of devs
 
 ## Cardápio online do restaurante.
+
+<h4 align="center"> 
+	🚀 ... Em desenvolvimento ... 🚀
+</h4>
+
+### HTML
+
+- [x] Header
+- [x] Footer
+- [ ] Main
--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -1,8 +1,28 @@
 import "./footer.css"
+import { Facebook, Instagram, Telephone, Whatsapp } from 'react-bootstrap-icons';
 
 function Footer() {
   return (
     <footer className="footer">
+      <div className="footer-div">
+        <div className="footer-div-endereco">
+          <h3>Endereço</h3>
+          <label>Rua dos zumbis, S\N - Raccoon City - Umbrella</label>
+          <label><Telephone /> (12) 3456-7890</label>
+          <label><Whatsapp /> (12) 9 1234-5678</label>
+        </div>
+        <div className="footer-div-funcionamento">
+          <h3>Horario de Funcionamento</h3>
+          <label>Segunda a sexta: 11:00 as 15:00 e 17:00 as 23:00</label>
+          <label>Sábado: 11:00 as 14:00 e 18:00 as 00:00</label>
+          <label>Domingo: Fechado</label>
+        </div>
+        <div className="footer-div-redessociais">
+          <h3>Redes sociais</h3>
+          <a href="https://www.facebook.com"><Facebook /> Facebook</a>
+          <a href="https://www.instagram.com"><Instagram /> Instagram</a>
+        </div>
+      </div>
     </footer>
   )
 }
